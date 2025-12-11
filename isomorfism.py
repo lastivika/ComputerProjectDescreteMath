cdb408c6fefc7c40efed47f398422acd1c0c7de9
admin <admin@admins-MacBook-Pro.local>
2025-12-11 17:46:25 +0200

    Вроді все окрім документації

--- isomorfism.py
+++ isomorfism.py
@@ -10,11 +10,11 @@ def check_edges(graph1: dict, graph2: dict) -> bool:
     edges_list2 = []
     for key in graph1.keys():
         for vertex in graph1[key]:
-            edges_list1.append(frozenset({key, vertex}))
+            edges_list1.append((key, vertex))
 
     for key in graph2.keys():
         for vertex in graph2[key]:
-            edges_list2.append(frozenset({key, vertex}))
+            edges_list2.append((key, vertex))
 
     return len(set(edges_list1)) == len(set(edges_list2))
 
@@ -38,7 +38,7 @@ def check_degrees(graph1: dict, graph2: dict) -> bool:
 
 
 def check_connection(graph: dict) -> bool:
-    all_connections_list = []
+    '''all_connections_list = []
     used_vertices = set()
     def find_connections(graph: dict, vertex) -> list:
         if vertex not in used_vertices:
@@ -55,12 +55,39 @@ def check_connection(graph: dict) -> bool:
 
     if len(set(all_connected_vertices)) == len(list(graph.keys())):
         return True
-    return False
+    return False'''
+
+
+    def dfs_recursive(graph, u, visited):
+        """Обхід у глибину для перевірки зв'язності (потрібно для is_bridge)."""
+        visited.add(u)
+        for v in graph.get(u, []):
+            if v not in visited:
+                dfs_recursive(graph, v, visited)
+
+
+    def get_unique_nodes(graph):
+        """Отримує всі унікальні вершини графа."""
+        nodes = set(graph.keys())
+        for neighbors in graph.values():
+            nodes.update(neighbors)
+        return nodes
+
+    nodes = get_unique_nodes(graph)
+    if not nodes:
+        return True
+
+    start_node = next(iter(nodes))
+    visited = set()
+    dfs_recursive(graph, start_node, visited)
+    active_nodes = {n for n in nodes if n in graph and len(graph[n]) > 0}
+    #active nodes не включають ізольовані
+    return len(visited) >= len(active_nodes)
 
 
 
 
-def breadth_first_search(graph: dict, vertex) -> dict:
+'''def breadth_first_search(graph: dict, vertex) -> dict:
     queue = [vertex]
     order = 0
     eccentricities = {vertex: 0}
@@ -74,14 +101,14 @@ def breadth_first_search(graph: dict, vertex) -> dict:
                 eccentricities[vertices] = eccentricities[vertex] + 1
                 queue.append(vertices)
 
-    return eccentricities
+    return eccentricities'''
 
 
 
 
 def find_diameter_of_graph(graph: dict) -> int | None:
-    '''if check_connection(graph) is False:
-        return None'''
+    if check_connection(graph) is False:
+        return None
 
     def breadth_first_search(graph: dict, vertex) -> dict:
         queue = [vertex]
@@ -92,10 +119,10 @@ def find_diameter_of_graph(graph: dict) -> int | None:
             vertex = queue[order]
             order += 1
 
-            for vertices in graph[vertex]:
-                if vertices not in eccentricities:
-                    eccentricities[vertices] = eccentricities[vertex] + 1
-                    queue.append(vertices)
+            for adjacent_vertex in graph[vertex]:
+                if adjacent_vertex not in eccentricities:
+                    eccentricities[adjacent_vertex] = eccentricities[vertex] + 1
+                    queue.append(adjacent_vertex)
 
         return eccentricities
 
@@ -108,7 +135,9 @@ def find_diameter_of_graph(graph: dict) -> int | None:
     return diameter
 
 
-def weisfeiler_lehman_test(graph: dict) -> bool:
+
+####################################################
+'''def weisfeiler_lehman_test(graph: dict) -> list:
     k = find_diameter_of_graph(graph)
 
     marks = []
@@ -136,30 +165,160 @@ def weisfeiler_lehman_test(graph: dict) -> bool:
 
         marks = new_marks
 
+    return marks'''
 
-    return marks
+def weisfeiler_lehman_test(graph_1: dict, graph_2: dict) -> list:
+    k = find_diameter_of_graph(graph_1)
+
+    marks_1 = {}
+    marks_2 = {}
+
+
+    for iteration in range(k):
+        if iteration == 0:
+            for vertex in graph_1:
+                marks_1[vertex] = len(graph_1[vertex])
+
+            for vertex in graph_2:
+                marks_2[vertex] = len(graph_2[vertex])
+
+        else:
+            marks_list_1 = sorted(marks_1)
+            marks_list_2 = sorted(marks_2)
+
+            if marks_list_1 != marks_list_2:
+                return False
+
+            new_marks_1 = {}
+            new_marks_2 = {}
+
+            for vertex in graph_1:
+                previous_mark_1 = graph_1[vertex]
+
+                adjacent_marks_1 = []
+                for vertices in graph_1[vertex]:
+                    adjacent_marks_1.append(vertices)
+
+                adjacent_marks_1 = sorted(adjacent_marks_1)
+
+                new_marks_1[vertex] = f'{previous_mark_1}{adjacent_marks_1}'
+
+
+            for vertex in graph_2:
+                previous_mark_2 = graph_2[vertex]
+
+                adjacent_marks_2 = []
+                for vertices in graph_2[vertex]:
+                    adjacent_marks_2.append(vertices)
+
+                adjacent_marks_2 = sorted(adjacent_marks_2)
+
+                new_marks_2[vertex] = f'{previous_mark_2}{adjacent_marks_2}'
+
+            marks_list_1 = new_marks_1
+            marks_list_2 = new_marks_2
+
+
+    return True
+
+
+
+
+
+
+
+
+
+
+
+####################################################
+
+
 
 
 def isomorfism_check(graph1: dict, graph2: dict) -> bool:
-    return all([check_vertices(graph1, graph2),
+    '''if all([check_vertices(graph1, graph2),
             check_edges(graph1, graph2),
             check_degrees(graph1, graph2),
-            check_connection(graph1) != check_connection(graph2),
-            find_diameter_of_graph(graph1) != find_diameter_of_graph(graph2),
-            weisfeiler_lehman_test(graph1) != weisfeiler_lehman_test(graph2)])
+            check_connection(graph1) == check_connection(graph2),
+            find_diameter_of_graph(graph1) == find_diameter_of_graph(graph2)]) is False:
+        print('FUCK')
+        return False
+
+
+    if weisfeiler_lehman_test(graph1, graph2) is False:
+        return False
+
+    return True'''
+
+    if check_vertices(graph1, graph2) is False:
+        return 'SHO NAHUi'
+
+    if check_edges(graph1, graph2) is False:
+        return 'NE VIRU'
+
+    if check_degrees(graph1, graph2) is False:
+        return 'NE MOZHE Bit'
+
+    if check_connection(graph1) == check_connection(graph2) is False:
+        return 'NE KONEKT'
+
+    if find_diameter_of_graph(graph1) == find_diameter_of_graph(graph2) is False:
+        return 'DIAMETER'
+
+    if weisfeiler_lehman_test(graph1, graph2) is False:
+        return 'NU NEEEEE'
+
+    return True
 
 
 
-
-
-print(weisfeiler_lehman_test({
-    1: {2, 3, 4},
-    2: {1, 5, 6},
-    3: {1, 4},
-    4: {1, 3},
-    5: {2, 6},
-    6: {2, 5},
+print(isomorfism_check(
+    {
+    1: [2, 5, 6],
+    2: [1, 3, 7],
+    3: [2, 4, 8],
+    4: [3, 5, 9],
+    5: [1, 4, 10],
+    6: [1, 11, 12],
+    7: [2, 13, 14],
+    8: [3, 15, 16],
+    9: [4, 17, 18],
+    10: [5, 19, 20],
+    11: [6],
+    12: [6],
+    13: [7],
+    14: [7],
+    15: [8],
+    16: [8],
+    17: [9],
+    18: [9],
+    19: [10],
+    20: [10]
+},
+    {
+    1:  [10, 19, 20],
+    2:  [9, 17, 18],
+    3:  [8, 15, 16],
+    4:  [7, 13, 14],
+    5:  [6, 11, 12],
+    6:  [5, 10, 9],
+    7:  [4, 8, 3],
+    8:  [3, 2, 1],
+    9:  [2, 1, 4],
+    10: [1, 6, 5],
+    11: [5],
+    12: [5],
+    13: [4],
+    14: [4],
+    15: [3],
+    16: [3],
+    17: [2],
+    18: [2],
+    19: [1],
+    20: [1]
 }))
+
 
 
 G = {
@@ -182,9 +341,6 @@ G1 = {
 }
 
 '''print(weisfeiler_lehman_test(G1))'''
-
-
-
 '''print(find_diameter_of_graph(G1))'''
 '''print(max(breadth_first_search(G1, 1).values()))'''
 '''print(check_connection(G1))'''
